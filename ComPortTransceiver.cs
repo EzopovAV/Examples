c8f1d8bede95c084534849c592937beaf63d2748
EzopovAV <59119037+EzopovAV@users.noreply.github.com>
2022-07-15 14:16:55 +0200

    Add files via upload

---
+++ ComPortTransceiver.cs
@@ -0,0 +1,34 @@
+﻿using ComPortDeviceCommunicate.Interfaces;
+using System;
+using System.IO.Ports;
+
+namespace ComPortDeviceCommunicate
+{
+	class ComPortTransceiver : IComPortTransceiver
+	{
+		public byte[] BytesToWrite { get; private set; }
+		public byte[] ReceivedBytes { get; private set; }
+
+		public event Action BytesToWriteChanged;
+		public event Action ReceivedBytesChanged;
+
+		public byte[] SendAndRead(SerialPort serialPort, byte[] bytesToWrite)
+		{
+			BytesToWrite = bytesToWrite;
+			BytesToWriteChanged?.Invoke();
+
+			var bytesToRead = new byte[bytesToWrite.Length];
+
+			for (int i = 0; i < bytesToWrite.Length; i++)
+			{
+				serialPort.Write(bytesToWrite, i, 1);
+				serialPort.Read(bytesToRead, i, 1);
+			}
+
+			ReceivedBytes = bytesToRead;
+			ReceivedBytesChanged?.Invoke();
+
+			return bytesToRead;
+		}
+	}
+}
---
+++ IComPortTransceiver.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO.Ports;
+
+namespace ComPortDeviceCommunicate.Interfaces
+{
+	public interface IComPortTransceiver
+	{
+		byte[] SendAndRead(SerialPort serialPort, byte[] bytesToWrite);
+		byte[] BytesToWrite { get; }
+		byte[] ReceivedBytes { get; }
+		event Action BytesToWriteChanged;
+		event Action ReceivedBytesChanged;
+	}
+}
